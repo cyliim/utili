83be7e1c65849040584cba0bd4e45fb5d1a986ac
utilibot <47805193+utilibot@users.noreply.github.com>
2019-03-02 13:05:20 +1300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var pun =["The first computer dates back to Adam and Eve. It was an Apple with l
 var ball =["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", " Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
 
 client.on('ready', () => {
-client.user.setActivity("brickman.glitch.me | $help"); 
+client.user.setActivity("utili.glitch.me | $help"); 
 });
 client.on("message", async (message) => {
     if (message.content.startsWith(`${prefix}help`)) {
