76d7e9f66fe90514c4cb74870891af788cf3323a
utilibot <47805193+utilibot@users.noreply.github.com>
2019-03-07 18:10:00 +1300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const prefix = "$";
 var rand = ["What is a sheep's favourite movie? ||Baaaaaa-ck to the future!||", "I hit my friend with a huge crystal of sodium chloride. ||I got arrested for a salt!||", "How do you add two numbers at the top of Mount Everest? ||Just summit.||", "Why did the dog say 'meow'? ||He was bilingual||", "There’s only one thing I don’t like about Halloween ||Which is...||", "Did you hear about the all-janitor baseball team? ||They swept the finals||", "A 300 page novel with a 50 page introductory essay written by the author walks into a bar. ||The bartender asks, 'Why the long preface?'||", "Why do gorillas have big nostrils? ||Because they have big fingers!||, What did the buffalo say to his son when he left for college? ||Bison||", "What is the best place to train your legs? ||Squatland Yard||"];
 var pun =["The first computer dates back to Adam and Eve. It was an Apple with limited memory, just one byte. And then everything crashed.", "About a month before he died, my uncle had his back covered in lard. After that, he went down hill fast.", "Doctor, there`s a patient on line 1 who says hes invisible. Doctor: Well I cant see him right now.", "Thanks for explaining the word many to me, it means a lot.", "I got a new pair of gloves today, but they're both lefts which, on the one hand, is great, but on the other, its just not right."];
 var ball =["It is certain..", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", " Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
-var fact =["fact1", "fact2"]
+var fact =["Sharks can’t breathe if they aren’t moving", "Saudi Arabia Imports Camels From Australia"]
 
 client.on('ready', () => {
 client.user.setActivity("utili.glitch.me | $help"); 
@@ -27,9 +27,10 @@ client.on("message", async (message) => {
             .addField("**Pun**", "Tells a pun.")
             .addField("**8ball**", "Acts like a magic 8ball.")
             .addField("**Avatar**", "Shows either your avatar or the avatar of the person you pinged.")
+            .addField("**Fact**", "Says a fun fact.")
             .addField("**Support**", "Join the support server [here](https://discord.gg/QHqJxMm).")
             .setTimestamp()
-            .setFooter("Created by brickman#4669", client.user.avatarURL);
+            .setFooter("Created by Brickman#4669", client.user.avatarURL);
         message.channel.send(embed);
     } else if (message.content.startsWith(`${prefix}flip`)) {
         message.channel.send(Math.floor(Math.random() * 2) === 0 ? "Heads" : "Tails");
