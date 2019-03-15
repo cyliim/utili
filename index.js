484aa44781cf519242627bfbe6fbdd4a88b537e3
utilibot <47805193+utilibot@users.noreply.github.com>
2019-03-15 18:47:21 +1300

    Update index.js

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ client.on("message", async (message) => {
         //info
             
     } else if (message.content.startsWith(`${prefix}info`)) {
-        message.author.send("Utili is currently on version 1.0.1.");
+        message.author.send("Utili is currently on version 1.0.2.");
         message.reply("sent you a dm!")
         }});
         client.login(token).catch(err => console.log(err));
