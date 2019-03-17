a34aeabdc63193f8a470043cac917ecc67773351
utilibot <47805193+utilibot@users.noreply.github.com>
2019-03-17 17:26:21 +1300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ client.on('ready', () => {
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); 
         client.user.setActivity(activities_list[index]); 
-    }, 60000); 
+    }, 30000); 
 });
 
 //Brickman says hi!
