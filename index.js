d15be5fe761a75e68fa7d308ac8364fe2caf53a7
utilibot <47805193+utilibot@users.noreply.github.com>
2019-03-19 15:57:42 +1300

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const activities_list = [
     ">help", 
     "with you!", 
     "a game",
-    "with " + client.guilds.size + " users!",
+    "with " + client.guilds.length + " servers!",
 
     ]; // creates an arraylist containing phrases you want your bot to switch through.
 
