246da9ab72d326fa02ab4c887733ff6013b78759
Brickman <47805193+utilibot@users.noreply.github.com>
2019-03-31 16:20:29 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const token = process.env.token;
-const args = message.content.split(" ").slice(1);
-const command = args.shift().toLowerCase();
 const prefix = ">"
 var version = "1.2.15"
 //joke list
@@ -165,22 +163,8 @@ client.on("message", async (message) => {
             message.channel.send("<:utilicross:556723032400461824> You are not the developer!"); 
                 } else if (message.author.id === '443992049746968586') {
                 (message.channel.send("<:utilicross:556723032400461824> Nothing to see here just yet!")) 
-            } else if (message.content.startsWith(`${prefix}eval`)) {
-                      if (message.author.id !== '443992049746968586') {
-                      try {
-      const code = args.join(" ");
-      let evaled = eval(code);
- 
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
- 
-      message.channel.send(clean(evaled), {code:"xl"});
-    } catch (err) {
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-    }
-  }
-}
-    }});
+            
+    });
 
 
 
